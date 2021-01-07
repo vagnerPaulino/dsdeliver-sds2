b7d393e231807b784f6dfb7bdac6cf06d562b028
vagnerPaulino <vagnerpaulinop@gmail.com>
2021-01-07 08:33:42 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Hello from "./Hello";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }
